1ba2462895fc870f27ce5518bd13c5448cedf227
N.Fujita <naofuji.1220@gmail.com>
2021-02-13 00:12:15 +0900

    initial commit

--- Chapter01/problem_07.cpp
+++ Chapter01/problem_07.cpp
@@ -8,7 +8,7 @@ int main() {
 	std::vector<int> pair;  // ペアを保存
 
 	for (int i = 1; i <= 1000000; ++i) {
-		if ((div(div(i)) == i) && (div(i) != i)) {  // 友愛数の条件
+		if ((div(div(i)) == i) && (div(i) != i) && (div(i) <= 1000000)) {  // 友愛数の条件
 			auto itr = std::find(pair.begin(), pair.end(), i);  // 配列内の要素iを走査
 			if (itr == pair.end()) {  // 配列内に同じ数字がなかった場合
 				pair.push_back(i);  // 友愛数を追加
@@ -28,9 +28,13 @@ int main() {
 // 自分以外の約数の和を返す
 int div(int num) {
 	int sum = 0;
-	for (int i = 2; i < std::sqrt(num); ++i) {
-		if ((num % i) == 0)
-			sum += (i + num/i);
+	for (int i = 2; i <= std::sqrt(num); ++i) {
+		if ((num % i) == 0) {
+			if (i == num/i)  // numが平方数なら
+				sum += i;
+			else
+				sum += (i + num/i);
+		}
 	}
 	return sum + 1;
 }
