50365671be6c8f5490e396ed7da6ed91fe12c711
N.Fujita <naofuji.1220@gmail.com>
2021-02-12 23:51:36 +0900

    initial commit

--- Chapter01/problem_07.cpp
+++ Chapter01/problem_07.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include <cmath>
-#include <set>
+#include <vector>
 
 
 int main() {
 	int div(int num);
-	std::set<int> pair;  // 重複しないようにペアを保存
+	std::vector<int> pair;  // 重複しないようにペアを保存
 
-	for (int i = 1; i <= 100000; ++i) {
-		if ((div(div(i)) == i) && (div(i) != i)) {
-			pair.insert(i);  // 友愛数を追加
-			pair.insert(div(i));  // 友愛数を追加
+	for (int i = 1; i <= 1000000; ++i) {
+		if ((div(div(i)) == i) && (div(i) != i)) {  // 友愛数の条件
+			auto itr = std::find(pair.begin(), pair.end(), i);  // 配列内の要素iを走査
+			if (itr == pair.end()) {  // 配列内に同じ数字がなかった場合
+				pair.push_back(i);  // 友愛数を追加
+				pair.push_back(div(i));  // 友愛数を追加
+			}
 		}
 	}
-	
-	auto itr = pair.begin();  // 最初の要素のイテレータを取得
-	for (int j = 0; j < pair.size()/2; ++j) {
-		std::cout << "(" << *itr << ", ";
-		++itr;
-		std::cout << *itr << ")" << '\n';
-		++itr;
-	}
+
+	for (int j = 0; j < pair.size(); j += 2)
+		std::cout << "(" << pair[j] << ", " << pair[j+1] << ")" << '\n';
+
 	std::cout << "は友愛数です" << '\n';
 	return 0;
 }
