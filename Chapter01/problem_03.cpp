bf68a1d1f0c1314cecdba9eda2bba86c56410465
N.Fujita <naofuji.1220@gmail.com>
2021-02-16 20:36:58 +0900

    initial commit

---
+++ Chapter01/problem_03.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+int min(int a, int b);
+
+ 
+int main() {
+	int num1, num2;  // 入力する値
+	int greatest = 0;  // 最大公約数
+
+	std::cout << "二つの正の整数を入力:" << '\n';
+	std::cin >> num1 >> num2;
+
+	for (int i = 1; i <= min(num1, num2); ++i) {
+		if ((num1 % i == 0) && (num2 % i == 0))
+			greatest = i;  
+	}
+	
+	int least = greatest * (num1/greatest) * (num2/greatest);  // 最小公倍数
+	std::cout << least << '\n';
+	return 0;
+}
+
+
+// 小さい方の整数を選ぶ
+int min(int a, int b) {
+	if (a <= b)
+		return a;
+	else
+		return b;
+}
+
+
+
+
